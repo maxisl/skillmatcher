b088c92564473eb1da21293039f187f666f413fc
SimonBurmer <40640328+SimonBurmer@users.noreply.github.com>
2022-11-21 12:15:27 +0100

    Initial backend commit

---
+++ backend/src/test/kotlin/mobileifi/Backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package mobileifi.Backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
