1585ecf82f8eb5d45938ff6a3f4dd0009d26013b
SimonBurmer <40640328+SimonBurmer@users.noreply.github.com>
2022-11-26 02:10:57 +0100

    Added wokring MySQL backend

---
+++ backend/src/test/kotlin/io/github/manuelernesto/playersapi/DemoPlayersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.manuelernesto.playersapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoPlayersApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
