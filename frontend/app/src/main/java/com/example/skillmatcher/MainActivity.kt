ed40aab830b4f1d47424bcd3c80aa72a3f3c6fdc
ra36giy <Aron1995>
2022-11-28 03:03:16 +0100

    bug fix

--- frontend/app/src/main/java/com/example/skillmatcher/MainActivity.kt
+++ frontend/app/src/main/java/com/example/skillmatcher/MainActivity.kt
@@ -1,47 +1,94 @@
 package com.example.skillmatcher
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import com.example.skillmatcher.destinations.PostScreenDestination
+import com.example.skillmatcher.destinations.ProfileScreenDestination
 import com.example.skillmatcher.ui.theme.SkillMatcherTheme
-import androidx.navigation.compose.rememberNavController
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import java.time.LocalDateTime
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SkillMatcherTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Navigation()
-                }
+                SkillMatcherTheme() {
+                DestinationsNavHost(navGraph = NavGraphs.root)
             }
-
         }
-
     }
 }
 
+@Destination(start = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LoginScreen(
+    navigator: DestinationsNavigator
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Login Screen")
+        Button(onClick = {
+            navigator.navigate(
+                ProfileScreenDestination(
+                    User(
+                        name = "Chris P. Bacon",
+                        id = "userid",
+                        created = LocalDateTime.now()
+                    )
+                )
+            )
+        }) {
+            Text("Go to Profile Screen")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Destination
 @Composable
-fun DefaultPreview() {
-    SkillMatcherTheme {
-        Greeting("Android")
+fun ProfileScreen(
+    navigator: DestinationsNavigator,
+    user: User
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Profile Screen: $user", textAlign = TextAlign.Center)
+        Button(onClick = {
+            navigator.navigate(PostScreenDestination())
+        }) {
+            Text("Go to Post Screen")
+        }
+    }
+}
+
+@Destination
+@Composable
+fun PostScreen(
+    showOnlyPostsByUser: Boolean = false
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Post Screen, $showOnlyPostsByUser")
     }
 }
