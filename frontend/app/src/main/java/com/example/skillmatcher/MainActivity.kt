aad8b4105b1c8fd03f692dde50823c980a03a351
SimonBurmer <40640328+SimonBurmer@users.noreply.github.com>
2022-12-13 20:32:11 +0100

    Added fancy navbar

--- frontend/app/src/main/java/com/example/skillmatcher/MainActivity.kt
+++ frontend/app/src/main/java/com/example/skillmatcher/MainActivity.kt
@@ -1,88 +1,93 @@
 package com.example.skillmatcher
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.skillmatcher.components.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.skillmatcher.R
 import com.example.skillmatcher.ui.theme.SkillMatcherTheme
-import com.example.skillmatcher.views.ProjectCreationPage
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+import com.example.skillmatcher.components.*
+
+/*
+You can use the following code for commercial purposes with some restrictions.
+Read the full license here: https://semicolonspace.com/semicolonspace-license/
+For more designs with source code, visit:
+https://semicolonspace.com/jetpack-compose-samples/
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SkillMatcherTheme {
-                AppMainScreen()
-            }
-        }
-    }
-}
+            SkillMatcherTheme(darkTheme = false) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun AppMainScreen() {
-    val navController = rememberNavController()
-    Surface(color = MaterialTheme.colors.background) {
-        val drawerState = rememberDrawerState(DrawerValue.Closed)
-        val scope = rememberCoroutineScope()
-        val openDrawer = {
-            scope.launch {
-                drawerState.open()
-            }
-        }
-        ModalDrawer(
-            drawerState = drawerState,
-            gesturesEnabled = drawerState.isOpen,
-            drawerContent = {
-                Drawer(
-                    onDestinationClicked = { route ->
-                        scope.launch {
-                            drawerState.close()
+                    val scaffoldState = rememberScaffoldState()
+                    val coroutineScope = rememberCoroutineScope()
+                    val contextForToast = LocalContext.current.applicationContext
+
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        scaffoldState = scaffoldState,
+                        topBar = {
+                            MyTopAppBar {
+                                coroutineScope.launch {
+                                    scaffoldState.drawerState.open()
+                                }
+                            }
+                        },
+                        drawerContent = {
+                            DrawerContent { itemLabel ->
+                                Toast
+                                    .makeText(contextForToast, itemLabel, Toast.LENGTH_SHORT)
+                                    .show()
+                                coroutineScope.launch {
+                                    // delay for the ripple effect
+                                    delay(timeMillis = 250)
+                                    scaffoldState.drawerState.close()
+                                }
+                            }
                         }
-                        navController.navigate(route) {
-                            popUpTo = navController.graph.startDestinationId
-                            launchSingleTop = true
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(text = "Rest of the App UI Here")
                         }
                     }
-                )
-            }
-        ) {
-            NavHost(
-                navController = navController,
-                startDestination = DrawerScreens.Home.route
-            ) {
-                composable(DrawerScreens.Home.route) {
-                    Home(
-                        openDrawer = {
-                            openDrawer()
-                        }
-                    )
-                }
-                composable(DrawerScreens.CreateProject.route) {
-                    ProjectCreationPage(
-                        openDrawer = {
-                        openDrawer()
-                    })
-                }
-                composable(DrawerScreens.Help.route) {
-                    Help(
-                        navController = navController
-                    )
                 }
             }
         }
     }
-
-
-
 }
---
+++ frontend/app/src/main/java/com/example/skillmatcher/components/fancydraw.kt
@@ -0,0 +1,210 @@
+package com.example.skillmatcher.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.skillmatcher.R
+
+
+@Composable
+ fun MyTopAppBar(onNavIconClick: () -> Unit) {
+    TopAppBar(
+        title = { Text(text = "SemicolonSpace") },
+        navigationIcon = {
+            IconButton(
+                onClick = {
+                    onNavIconClick()
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Open Navigation Drawer"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+ fun DrawerContent(
+    gradientColors: List<Color> = listOf(Color(0xFFF70A74), Color(0xFFF59118)),
+    itemClick: (String) -> Unit
+) {
+
+    val itemsList = prepareNavigationDrawerItems()
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = Brush.verticalGradient(colors = gradientColors)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        contentPadding = PaddingValues(vertical = 36.dp)
+    ) {
+
+        item {
+
+            // user's image
+            Image(
+                modifier = Modifier
+                    .size(size = 120.dp)
+                    .clip(shape = CircleShape),
+                painter = painterResource(id = R.drawable.android_icon),
+                contentDescription = "Profile Image"
+            )
+
+            // user's name
+            Text(
+                modifier = Modifier
+                    .padding(top = 12.dp),
+                text = "Hermione",
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+
+            // user's email
+            Text(
+                modifier = Modifier.padding(top = 8.dp, bottom = 30.dp),
+                text = "hermione@email.com",
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+                color = Color.White
+            )
+        }
+
+        items(itemsList) { item ->
+            NavigationListItem(item = item) {
+                itemClick(item.label)
+            }
+        }
+    }
+}
+
+@Composable
+private fun NavigationListItem(
+    item: NavigationDrawerItem,
+    unreadBubbleColor: Color = Color(0xFF0FFF93),
+    itemClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+                itemClick()
+            }
+            .padding(horizontal = 24.dp, vertical = 10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        // icon and unread bubble
+        Box {
+
+            Icon(
+                modifier = Modifier
+                    .padding(all = if (item.showUnreadBubble && item.label == "Messages") 5.dp else 2.dp)
+                    .size(size = if (item.showUnreadBubble && item.label == "Messages") 24.dp else 28.dp),
+                painter = item.image,
+                contentDescription = null,
+                tint = Color.White
+            )
+
+            // unread bubble
+            if (item.showUnreadBubble) {
+                Box(
+                    modifier = Modifier
+                        .size(size = 8.dp)
+                        .align(alignment = Alignment.TopEnd)
+                        .background(color = unreadBubbleColor, shape = CircleShape)
+                )
+            }
+        }
+
+        // label
+        Text(
+            modifier = Modifier.padding(start = 16.dp),
+            text = item.label,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.White
+        )
+    }
+}
+
+@Composable
+private fun prepareNavigationDrawerItems(): List<NavigationDrawerItem> {
+    val itemsList = arrayListOf<NavigationDrawerItem>()
+
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.android_icon),
+            label = "Home"
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.android_icon),
+            label = "Messages",
+            showUnreadBubble = true
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.android_icon),
+            label = "Notifications",
+            showUnreadBubble = true
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.android_icon),
+            label = "Profile"
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.android_icon),
+            label = "Payments"
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.android_icon),
+            label = "Settings"
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.android_icon),
+            label = "Logout"
+        )
+    )
+
+    return itemsList
+}
+
+data class NavigationDrawerItem(
+    val image: Painter,
+    val label: String,
+    val showUnreadBubble: Boolean = false
+)
