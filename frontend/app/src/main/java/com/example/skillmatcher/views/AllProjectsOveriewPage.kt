1ca3ceac9c7361e830bc7f91e8ba2878a3a4b996
nsk1298 <navi.kumar@campus.lmu.de>
2022-12-16 12:37:32 +0100

    Project overview page added

--- frontend/app/src/main/java/com/example/skillmatcher/views/AllProjectsOveriewPage.kt
+++ frontend/app/src/main/java/com/example/skillmatcher/views/AllProjectsOveriewPage.kt
@@ -1,25 +1,180 @@
 package com.example.skillmatcher
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material3.TabRowDefaults.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.skillmatcher.destinations.LandingPageDestination
 import com.example.skillmatcher.destinations.VisitorsProjectOverviewPageDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.launch
+import java.time.format.TextStyle
 
+
+@Preview
 @Destination
 @Composable
 fun AllProjectsOverViewPage(
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Post Enis")
+    Column(modifier = Modifier.fillMaxSize()) {
+        HeadBar(
+            name = "Create Project", modifier = Modifier
+                .padding(5.dp)
+        )
+        Divider(color= Color.Black, thickness = 1.dp)
+        Spacer(modifier = Modifier.height(4.dp))
+        ProjectDetailsHeader()
+
     }
 }
+@Composable
+fun HeadBar(
+    name:String,
+    modifier: Modifier=Modifier
+){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier =modifier.fillMaxWidth()
+    ){
+        androidx.compose.material3.Text(
+            text = name,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            color = Color.Blue
+        )
+
+    }
+}
+
+@Composable
+fun ProjectDetails(
+    name:String,
+    description:String,
+    modifier: Modifier=Modifier
+){  Column(
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = modifier
+    ){
+        Spacer(modifier = Modifier.height(4.dp))
+        androidx.compose.material3.Text(
+            text = name,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp,
+            color = Color.Blue,
+        )
+    }
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ){
+        androidx.compose.material3.Text(
+            text = description,
+            fontSize = 25.sp
+        )
+    }
+}
+
+
+@Composable
+fun ProjectDetailsHeader(modifier: Modifier=Modifier){
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier.fillMaxWidth(2f)
+    ){
+        //hier muss die Datenbankverknüpfung her
+        ProjectDetails("Project 1", "Datenbanksystem mit SQL entwickeln")
+        Divider(color= Color.Black, thickness = 1.dp)
+
+    }
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier.fillMaxWidth(2f)
+    ){
+        //hier muss die Datenbankverknüpfung her
+        ProjectDetails("Project 2", "Frontend mit Compose entwickeln")
+        Divider(color= Color.Black, thickness = 1.dp)
+
+    }
+}
+
+
+//TODO wenn wir die Projecte haben
+/**@Composable
+fun AllProjectList(
+    projects: List<Project>,
+    modifier: Modifier =Modifier
+){
+    Box(modifier){
+        val listState= rememberLazyListState()
+        val scope= rememberCoroutineScope()
+        LazyColumn(state= listState,
+            contentPadding= PaddingValues(bottom=80.dp),
+            modifier=Modifier.fillMaxWidth()){
+            val grouped= projects.groupBy { it.firstName[0] }
+
+            grouped.forEach{initial, projects ->
+                stickyHeader{CharacterHeader(char= initial, Modifier.fillParentMaxWidth())}
+           items(projects) { project ->
+               ProjectListItem(
+                   project = project,
+                   modifier = Modifier.fillMaxWidth()
+               )
+                 }
+            }
+        }
+        val showButton=listState.firstVisibleItemIndex >0
+        AnimatedVisibility(visible = showButton,
+        enter= fadeIn(),
+            exit= fadeOut(),
+            modifier = Modifier.align(Alignment.BottomCenter)
+        ) {
+            ScrollToTopButton(onClick={
+                                      scope.launch {
+                                          listState.scrollToItem(0)
+                                      }
+            }, modifier=Modifier.padding(16.dp))
+        }
+
+    }
+}
+
+@Composable
+fun CharacterHeader(char: Unit, fillParentMaxWidth: Modifier) {
+
+}
+
+@Composable
+fun ProjectListItem(project: Project, modifier: Modifier) {
+
+}
+
+class Project {
+
+    val firstName: Any
+}
+*/
+
