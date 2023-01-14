2c13aeebf4a44e70351186796f535acb86ef3a6d
nsk1298 <navi.kumar@campus.lmu.de>
2023-01-14 14:39:52 +0100

    Datenbank first Attempt

--- backend/src/main/kotlin/restapi/model/Project.kt
+++ backend/src/main/kotlin/restapi/model/Project.kt
@@ -35,29 +35,39 @@ data class Project(
     @NotBlank(message = "Description is mandatory")
     val description: String,
 
+    //Wie viele reindürfen
     @JsonView(DataView.Project::class)
     @NotBlank(message = "MaxAttendees is mandatory")
     val maxAttendees: String,
 
-    // val skillsNeeded: String,
+    @OneToMany(mappedBy = "Project", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var projectUser:Set<ProjectUser>,
+
+    @OneToMany(mappedBy = "Project", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var projectSkill:Set<ProjectSkill>
+
+
+        // val skillsNeeded: String,
 
     // val startDate: Date,
 
     // val endDate: Date,
 
-    // A User can be the owner of many projects
+  /**  // A User can be the owner of many projects
     @JsonView(DataView.ProjectWithOwner::class)             // specify which fields should be included in the JSON representation of the object
     @ManyToOne(fetch = FetchType.EAGER, optional = false)   // a single ApiUser object can be related to multiple Project objects
     @JoinColumn(name = "owner_id", nullable = true)         // specifies the name of the column in the Project table to store the foreign key pointing to the ApiUser object
     @OnDelete(action = OnDeleteAction.CASCADE)              // when an ApiUser object is deleted, any related Project objects should also be deleted
-    var owner: ApiUser?,
+    var owner: ApiUser?,*/
 
-    // Many user attend many projects
+   /** // Many user attend many projects
     @JsonView(DataView.ProjectWithAttendeesAndOwner::class)             // a single Project object can be related to multiple ApiUser objects, and a single ApiUser object can be related to multiple Project objects
     @ManyToMany(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)    // specifies the name of the join table that will be used to store the many-to-many relationship between Project and ApiUser objects
     @JoinTable(
         name = "project_attendees",                                     // name of the join table that will be used to store the many-to-many relationship between Project and ApiUser objects
         joinColumns = [JoinColumn(name = "project_id")],                // names of the columns in the join table that will store the foreign keys to the Project and ApiUser tables
         inverseJoinColumns = [JoinColumn(name = "user_id")])
+    @OrderColumn(name = "id")
     var attendees: MutableList<ApiUser>?                                // attendees field is a nullable list of ApiUser objects
+     */
 )
---
+++ backend/src/main/kotlin/restapi/model/ProjectUser.kt
@@ -0,0 +1,25 @@
+package restapi.model
+
+import com.fasterxml.jackson.annotation.JsonView
+import lombok.Data
+import restapi.jsonView.DataView
+import javax.persistence.*
+
+@Entity
+@Data
+@Table(name = "ProjectUser")
+class ProjectUser {
+
+    @JsonView(DataView.UserWithProjects::class)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var projectUser_id: Long? = null
+
+    @ManyToOne
+    @JoinColumn(name="User_id")
+    lateinit var user:ApiUser
+
+    @ManyToOne
+    @JoinColumn(name="Project_id")
+    lateinit var project: Project
+}
